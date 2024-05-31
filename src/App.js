660a59e262f12563789c81fd75bf266e7ad47024
braianbarraza <braian_019@hotmail.com>
2024-05-31 15:42:51 +0200

    third-change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import logoNav from "./img/Icon-Nav.png";
+import headerImg from "./img/Img_Header.png";
 
 const navigation = [
-  { name: "Products", href: "#" },
-  { name: "Features", href: "#" },
-  { name: "Marketplace", href: "#" },
-  { name: "Company", href: "#" },
+  { name: "Home", href: "#" },
+  { name: "Spaces", href: "#" },
+  { name: "Prices", href: "#" },
+  { name: "Contact", href: "#" },
+  { name: "About us", href: "#" },
 ];
 
 export default function Example() {
@@ -22,11 +25,7 @@ export default function Example() {
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
+              <img className="h-8 w-auto" src={logoNav} alt="" />
             </a>
           </div>
           <div className="flex lg:hidden">
@@ -37,20 +36,6 @@ export default function Example() {
             >
               <span className="sr-only">Open main menu</span>
               <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3"
-                />
-              </svg>
             </button>
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
@@ -121,6 +106,9 @@ export default function Example() {
                 </div>
               </div>
             </div>
+            <div className="flex items-center justify-between">
+              <img className="w-10 h-10" src={headerImg} alt="" />
+            </div>
           </DialogPanel>
         </Dialog>
       </header>
@@ -138,7 +126,7 @@ export default function Example() {
             }}
           />
         </div>
-        <div className="flex flex-col items-center mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="flex flex-col items-center mx-auto max-w-2xl py-20 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Announcing our next round of funding.{" "}
